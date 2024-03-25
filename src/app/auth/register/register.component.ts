3c3d8cfccea7dccb37915ccaf22a33fe52f8bfca
mtonon <math.tonon@gmail.com>
2024-03-25 06:53:32 -0300

    feat: set reactive form and bind inputs

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styles: ``
 })
 export class RegisterComponent {
+  form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit() {
+    console.log(this.form.getRawValue());
+  }
 }
