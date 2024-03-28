8c20d94c95639c32e2574849024f50003cfe895b
mtonon <math.tonon@gmail.com>
2024-03-28 06:57:31 -0300

    feat: add persistance service to handle local storage throught the application

--- src/app/store/auth.effects.ts
+++ src/app/store/auth.effects.ts
@@ -5,20 +5,28 @@ import { authActions } from './auth.actions';
 import { catchError, map, of, switchMap } from 'rxjs';
 import { ICurrentUser } from '../shared/interfaces/current-user.interface';
 import { HttpErrorResponse } from '@angular/common/http';
+import { PersistanceService } from '../shared/services/persistance.service';
 
 export const registerEffect = createEffect(
-  (actions$ = inject(Actions), authService = inject(AuthService)) => {
+  (
+    actions$ = inject(Actions),
+    authService = inject(AuthService),
+    persistanceService = inject(PersistanceService)
+  ) => {
     return actions$.pipe(
       ofType(authActions.register),
       switchMap(({ request }) => {
         return authService.register(request).pipe(
           map((currentUser: ICurrentUser) => {
+            persistanceService.set('accessToken', currentUser.token);
             return authActions.registerSuccess({ currentUser });
           }),
           catchError((errorResponse: HttpErrorResponse) => {
-            return of(authActions.registerFailure({
-              errors: errorResponse.error.errors
-            }));
+            return of(
+              authActions.registerFailure({
+                errors: errorResponse.error.errors,
+              })
+            );
           })
         );
       })
