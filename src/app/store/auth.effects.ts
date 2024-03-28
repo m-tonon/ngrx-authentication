b0ab0af612010a1850377788efa8150acde2f4a2
mtonon <math.tonon@gmail.com>
2024-03-28 07:01:54 -0300

    feat: add effect to handle redirection after registration

--- src/app/store/auth.effects.ts
+++ src/app/store/auth.effects.ts
@@ -2,10 +2,11 @@ import { inject } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { AuthService } from '../auth/services/auth.service';
 import { authActions } from './auth.actions';
-import { catchError, map, of, switchMap } from 'rxjs';
+import { catchError, map, of, switchMap, tap } from 'rxjs';
 import { ICurrentUser } from '../shared/interfaces/current-user.interface';
 import { HttpErrorResponse } from '@angular/common/http';
 import { PersistanceService } from '../shared/services/persistance.service';
+import { Router } from '@angular/router';
 
 export const registerEffect = createEffect(
   (
@@ -33,4 +34,16 @@ export const registerEffect = createEffect(
     );
   },
   { functional: true }
+);
+
+export const redirectAfterRegisterEffect = createEffect(
+  (actions$ = inject(Actions), router = inject(Router)) => {
+    return actions$.pipe(
+      ofType(authActions.registerSuccess),
+      tap(() => {
+        router.navigateByUrl('/');
+      })
+    );
+  },
+  { functional: true, dispatch: false }
 );
